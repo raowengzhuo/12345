f807d50646ca5919ac5dd612a9184780691d35c6
raowengzhuo <2924796878@qq.com>
2021-08-21 22:07:07 +0800

    zy

--- 0.1.cpp
+++ 0.1.cpp
@@ -1,5 +1,24 @@
-#include<stdio.h>
-int main(){
-    printf("hello");
+#include <stdio.h>
+int main()
+{
+    int m, n;
+    scanf("%d %d", &m, &n);
+    int k = 0;
+    k = m;
+    if (m < n)
+    {
+
+        m = n;
+        n = k;
+    }
+    int l;
+    while (k != 0)
+    {
+        l = n;     //上一级余数
+        k = m % n; //现在的余数
+        m = n;
+        n = k;
+    }
+    printf("最大公约数为：%d\n", l);
     return 0;
-}
+} //测试样例：1997 615 公约数为：1
