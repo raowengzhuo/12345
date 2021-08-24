526f9e90c4535539c863989a6e9d473daf68f98a
raowengzhuo <2924796878@qq.com>
2021-08-24 19:32:33 +0800

    最长连续递增子序列

---
+++ 1.02.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n], i;
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    int count = 0, max = 0, flag = 0, position = 0;
+    for (i = 0; i < n; i++)
+    {
+        if (count == 0)
+            count++;
+        else if (count > 0)
+        {
+            if (a[i] > a[i - 1])
+                count++;
+            else
+            {
+                if (count > max)
+                {
+                    position = i - count;
+                    max = count;
+                }
+                count = 0;
+                i--;
+            }
+        }
+        if (i == n - 1)
+        {
+            if (count > max)
+            {
+                position = i - count + 1;
+                max = count;
+            }
+        }
+    }
+    printf("%d", a[position]);
+    for (i = position + 1; i < position + max; i++)
+        printf(" %d", a[i]);
+    return 0;
+}
